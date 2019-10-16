08860b1b6a4d4ff6eaed01cfabdb0a01b88a6a50
Lauren Dove <elledoveinc@gmail.com>
2019-10-16 00:47:04 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,7 +8,9 @@ class Scraper
   
   def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-    binding.pry
+    #binding.pry
+    
+    
   end
   
   Scraper.new.get_page
